5f82db166fb23c6c9c0a8f867b75a440bf18b2ca
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-05-26 11:52:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-//complete this code
+// Base Person class
 class Person {
-	constructor(name, age)
-	{
-		this._name=name;
-		this._age=age;
-	}
-	get name()
-	{
-		return this._name;
-	}
-	set age(newAge)
-	{
-		this._age=newAge;
-	}
-	  get age() {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for age
+  get age() {
     return this._age;
+  }
+
+  // Setter for age
+  set age(newAge) {
+    this._age = newAge;
   }
 }
 
+// Student subclass
 class Student extends Person {
-	study()
-	{
-		console.log(this._name," is studying");
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
+// Teacher subclass
 class Teacher extends Person {
-	teach()
-	{
-		console.log(this._name," is teaching");
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
 // Do not change the code below this line
